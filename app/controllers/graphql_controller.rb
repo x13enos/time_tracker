9badcadb53555262b8b70636e355910858b2f189
Andres Sild <x3enos@gmail.com>
2019-10-13 14:42:37 +0300

    TT-20: put user's tokens into the cookies instead of passing them to client

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,6 +1,10 @@
+include ActionController::Cookies
+
 class GraphqlController < ApplicationController
   def execute
-    result = Graphql::SchemaExecutor.new(params, request.headers).perform
+    find_user
+    result = Graphql::SchemaExecutor.new(params, @user).perform
+    set_new_token_to_cookies(result)
     render json: result
   rescue => e
     raise e unless Rails.env.development?
@@ -8,6 +12,20 @@ class GraphqlController < ApplicationController
   end
 
   private
+
+  def find_user 
+    @user = Graphql::UserFinder.new(cookies[:token]).perform
+  end
+
+  def set_new_token_to_cookies(result)
+    token = Graphql::TokenSelector.new(@user, result).perform
+    return unless token
+    cookies[:token] = {
+      value: token,
+      secure: Rails.env.production?,
+      httponly: true
+    }
+  end
 
   def handle_error_in_development(e)
     logger.error e.message
