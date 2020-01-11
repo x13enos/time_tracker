2208b3a9880d65fb7ad0a22e518d2fb7a644257d
Andres Sild <x3enos@gmail.com>
2020-01-11 16:39:53 +0200

    TT-74: updated report template

--- app/services/report_generator.rb
+++ app/services/report_generator.rb
@@ -14,18 +14,29 @@ class ReportGenerator
   attr_reader :params
 
   def select_time_records
-    @time_records = params[:user].time_records.
+    @time_records = params[:user].time_records.includes(:project).
       where("assigned_date BETWEEN ? AND ?", params[:from_date], params[:to_date]).
       order(created_at: :desc)
   end
 
   def generate_report
-    html_string = ActionController::Base.render(file: "pdfs/report", locals: { time_records: @time_records })
-    pdf = WickedPdf.new.pdf_from_string(html_string)
+    pdf = WickedPdf.new.pdf_from_string(render_report_from_template)
     @file_name = "report-#{SecureRandom.uuid}.pdf"
     File.open(Rails.root.join('public/reports/', @file_name), 'wb') do |file|
       file << pdf
     end
+  end
+
+  def render_report_from_template
+    ActionController::Base.render(
+      file: "pdfs/report", locals: {
+        user: params[:user],
+        time_records: @time_records,
+        from_date: params[:from_date],
+        to_date: params[:to_date],
+        projects: @time_records.map(&:project).uniq
+      }
+    )
   end
 
   def link
--- spec/services/report_generator_spec.rb
+++ spec/services/report_generator_spec.rb
@@ -7,15 +7,16 @@ RSpec.describe ReportGenerator do
     let(:from_date) { Time.zone.local(2019, 10, 15) }
     let(:to_date) { Time.zone.local(2019, 10, 30) }
     let(:generator) { ReportGenerator.new({ user: user, from_date: from_date, to_date: to_date }) }
+    let(:project) { create(:project) }
 
     before do
       travel_to Time.zone.local(2019, 10, 29)
 
-      @time_record = create(:time_record, user: user, assigned_date: Time.zone.today, spent_time: 0.45)
-      @time_record_2 = create(:time_record, created_at: Time.zone.now - 2.hour, assigned_date: Time.zone.today, spent_time: 1.44)
-      @time_record_3 = create(:time_record, user: user, assigned_date: Time.zone.today)
-      @time_record_4 = create(:time_record, user: user, created_at: Time.zone.now - 1.hour, assigned_date: Time.zone.today - 10.days, spent_time: 3.5)
-      @time_record_5 = create(:time_record, user: user, assigned_date: Time.zone.today - 25.days)
+      @time_record = create(:time_record, project: project, user: user, assigned_date: Time.zone.today, spent_time: 0.45)
+      @time_record_2 = create(:time_record, project: project, created_at: Time.zone.now - 2.hour, assigned_date: Time.zone.today, spent_time: 1.44)
+      @time_record_3 = create(:time_record, project: project, user: user, assigned_date: Time.zone.today)
+      @time_record_4 = create(:time_record, project: project, user: user, created_at: Time.zone.now - 1.hour, assigned_date: Time.zone.today - 10.days, spent_time: 3.5)
+      @time_record_5 = create(:time_record, project: project, user: user, assigned_date: Time.zone.today - 25.days)
 
       travel_back
     end
@@ -25,7 +26,11 @@ RSpec.describe ReportGenerator do
       expect(ActionController::Base).to receive(:render).with({
         file: "pdfs/report",
         locals: {
-          time_records: [@time_record, @time_record_3, @time_record_4]
+          time_records: [@time_record, @time_record_3, @time_record_4],
+          projects: [project],
+          user: user,
+          from_date: from_date,
+          to_date: to_date
         }
       })
       generator.perform
