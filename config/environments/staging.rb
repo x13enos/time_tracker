53ccac4e227d23a3e89a4c93f824ffd0add8d4e1
Andres Sild <x3enos@gmail.com>
2020-04-26 14:05:16 +0300

    TT-121: change 'from' for emails, change name for env variable of smtp password

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
     port:                 ENV["SMTP_PORT"],
     domain:               ENV["SMTP_DOMAIN"],
     user_name:            ENV["SMTP_USERNAME"],
-    password:             ENV["PASSWORD"],
+    password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true }
   config.action_mailer.asset_host = ENV["HOST"]
