c1d1562047e3c56a438fd2155681eafa094a023a
Andres Sild <x3enos@gmail.com>
2020-05-06 20:41:39 +0300

    TT-115: validate regexp for projects

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,25 +6,41 @@ RSpec.describe Project, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).scoped_to(:workspace_id) }
 
+    describe "regexp_should_be_valid" do
+      it "should add error if passed regexp is invalid" do
+        project = build(:project, regexp_of_grouping: "/{)}+)(((/")
+        project.save
+        expect(project.errors[:regexp_of_grouping]).to eq([I18n.t("projects.errors.incorrect_format_of_regexp")])
+      end
+
+      it "should not have errors if passed regexp was correct" do
+        project = build(:project, regexp_of_grouping: "/\ATT-\d+/")
+        project.save
+        expect(project.errors[:regexp_of_grouping]).to be_empty
+      end
+    end
+  end
+
+  context 'associations' do
     it { should have_and_belong_to_many(:users) }
     it { should have_many(:time_records).dependent(:destroy) }
     it { should belong_to(:workspace) }
+  end
 
-    describe '#belongs_to_user?' do
-      it "should return true if that belongs to the passed user" do
-        user = create(:user)
-        project = create(:project)
-        project.users << user
+  describe '#belongs_to_user?' do
+    it "should return true if that belongs to the passed user" do
+      user = create(:user)
+      project = create(:project)
+      project.users << user
 
-        expect(project.belongs_to_user?(user.id)).to be_truthy
-      end
+      expect(project.belongs_to_user?(user.id)).to be_truthy
+    end
 
-      it "should return false if that doesn't belong to the passed user" do
-        user = create(:user)
-        project = create(:project)
+    it "should return false if that doesn't belong to the passed user" do
+      user = create(:user)
+      project = create(:project)
 
-        expect(project.belongs_to_user?(user.id)).to be_falsey
-      end
+      expect(project.belongs_to_user?(user.id)).to be_falsey
     end
   end
 
