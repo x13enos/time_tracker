bf8d29eafa39480e7eaf24c005794f9c540f4906
Andres Sild <x3enos@gmail.com>
2020-10-03 17:38:43 +0300

    TT-109: localised several emails, added logic of fetching locale from params

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,18 +3,18 @@ class UserMailer < ApplicationMailer
   def invitation_email(invitation_data)
     @invitation_data = invitation_data
     @token = TokenCryptService.encode(invitation_data[:user].email, 8.hours)
-    mail(to: invitation_data[:user].email, subject: "Welcome to the time tracker!")
+    mail(to: invitation_data[:user].email, subject: I18n.t("mailer.subjects.welcome"))
   end
 
   def recovery_password_email(user)
     @user = user
     @token = TokenCryptService.encode(user.email, 1.hours)
-    mail(to: user.email, subject: "Reset your Time Tracker account")
+    mail(to: user.email, subject: I18n.t("mailer.subjects.reset_token"))
   end
 
   def assigning_to_workspace_email(invitation_data)
     @invitation_data = invitation_data
-    mail(to: invitation_data[:user].email, subject: "You was assigned to new workspace!")
+    mail(to: invitation_data[:user].email, subject: I18n.t("mailer.subjects.you_were_assigned_to_workspace"))
   end
 
   def assign_user_to_project(user, project)
